7a684c4412179d296598eb526830c15bea477c44
Sujit02022013 <149417653+Sujit02022013@users.noreply.github.com>
2023-12-01 23:49:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+// 1. Return the Sum of Two Numbers
+// function addition(first, second){
+//     const sum =first + second;
+//     return sum;
+// }
+
+// console.log(addition(2,3));
+
+// console.log(addition(-3,-6));
+// console.log(addition(7,3));
+
+//2. Convert Minutes into Seconds
+// function minute(x){
+//     const second= x*60;
+//     return second;
+// }
+
+// console.log(minute(5));
+// console.log(minute(3));
+// console.log(minute(2));
+
+//3. Return the Next Number from the Integer Passed
+// arrow function
+// const nextNumber =(x) =>{
+//     return x + 1;
+// }
+
+// console.log(nextNumber(0));
+// console.log(nextNumber(9));
+// console.log(nextNumber(-3));
+
+// 4. Area of a Triangle
+// const triArea=(base,height)=>{
+//     return (base * height)/2;
+// }
+// console.log(triArea(3,2));
+// console.log(triArea(7,4));
+// console.log(triArea(10,10));
+
+//5. Return the First Element in an Array
+
+//const numberArray=[1,2,3];
+// const numberArray =[80, 5, 100];
+// function getFirstValue(numberArray){
+//     return numberArray[0];
+// }
+// console.log(numberArray[0]);
+
+//6. Convert Age to Days
+
+// 7. Power Calculator
+
+// function circuitPower(voltage, current){
+//     //power = voltage * current
+//     const power= voltage * current;
+//     return power;
+// }
+
+// console.log(circuitPower(230,10));
+// console.log(circuitPower(110,3));
+// console.log(circuitPower(480,20));
+
+//8. Maximum Edge of a Triangle
+
+// const thirdEdge =(side1, side2)=>{
+//     return side1 + side2 -1;
+// }
+// console.log(thirdEdge(8,10));
+// console.log(thirdEdge(5,7));
+// console.log(thirdEdge(9,2));
+
+// 9. Return the Remainder from Two Numbers
+
+// const remainder = (firstNumber, secondNumber)=>{
+//     return firstNumber % secondNumber;
+// }
+// console.log(remainder(1,3));
+// console.log(remainder(3,4));
+// console.log(remainder(-9,45));
+// console.log(remainder(5,5));
+
+//10. Return Something to Me!
+
+// const giveMeSomething = (word)=>{
+//     const result = "something " + word;
+//     return result;
+// }
+
+// console.log(giveMeSomething("is better than nothing"));
+// console.log(giveMeSomething("Bob Jane"));
+// console.log(giveMeSomething("something"));
+
+//11. Correct the Mistakes
+
+// const square=(number)=>{
+//     return number * number;
+// }
+
+// console.log(square(9));
+
+//12. Is the Number Less than or Equal to Zero?
+// Create a function that takes a number as its only argument and returns true
+//  if it's less than or equal to zero, otherwise return false
+
+// const validationNumber = (number)=>{
+//     if (number<=0){
+//         return true;
+//     } 
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(validationNumber(-5));
+
+//13. Sum of Polygon Angles
+
+// const sumPolygon=(angles)=>{
+//     return (angles-2)*180;
+// }
+// console.log(sumPolygon(6));
+
+//14. Basic Variable Assignment
+// const nameString = (word)=>{
+//     const result = word + "coder";
+//     return result;
+// }
+
+// console.log(nameString("Musabir"));
+// console.log(nameString("Matt"));
+// console.log(nameString("javascript"));
+
+//15. Less Than 100?
+// const lessThan100 = (number1, number2)=>{
+//     if (number1 + number2<100){
+//         return true;
+//     } 
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(lessThan100(85,20));
